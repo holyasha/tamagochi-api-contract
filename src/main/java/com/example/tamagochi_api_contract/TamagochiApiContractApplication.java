102ae336df95ccfc382f37c5b843aab86cd1ac20
holyasha <goou.price@gmail.com>
2026-03-16 12:33:06 +0300

    initial commit

---
+++ src/main/java/com/example/tamagochi_api_contract/TamagochiApiContractApplication.java
@@ -0,0 +1,13 @@
+package com.example.tamagochi_api_contract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamagochiApiContractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TamagochiApiContractApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tamagochi_api_contract/TamagochiApiContractApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tamagochi_api_contract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TamagochiApiContractApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
